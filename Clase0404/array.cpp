8a5f3b478771dfddb33f75fe0ca99995d87e313a
Erick <ertorreso@unal.edu.co>
2025-04-09 00:02:23 -0500

    updating array

--- Clase0404/array.cpp
+++ Clase0404/array.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <iostream>
 
+void print_v0(std::vector<double> mydata);
+void print_v1(std::vector<double> mydata);
+
 int main(void)
 {
     std::vector<double> data = {0.1, 0.5, -0.6};
